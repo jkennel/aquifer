140a21e7ae34b0f37599c502d1affb4358131e0f
jkennel <jkennel@uoguelph.ca>
2017-02-02 11:09:50 -0500

    update description for gamma_der

--- src/special_functions.cpp
+++ src/special_functions.cpp
@@ -46,7 +46,7 @@ double bh_gamma_neg(double u, double a) {
 //' gamma_der
 //'
 //' @description
-//' Derivative of the gamma function.  Need to check on this.
+//' Derivative of the gamma function.
 //'
 //' @param u numeric to evaluate the gamma function
 //' @param a numeric
